822c04489586954e711799dabef58e72f995cb14
takakikasuga <takaki.soccer1@gmail.com>
2022-05-20 08:50:14 +0900

    :sparkles: 引数をデコレーターを使用して取得できるように実装

--- expense-app/src/app.controller.ts
+++ expense-app/src/app.controller.ts
@@ -1,12 +1,20 @@
 // NOTE: Controllerの責務はエンドポイントを作成すること。
 
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { data, MappedReport } from './data';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    console.log('type === ', type);
+    const reportType =
+      type === MappedReport.expense
+        ? MappedReport.expense
+        : MappedReport.income;
+    return data.report.filter((report) => {
+      report.type === reportType;
+    });
   }
 
   @Get(':id')
--- expense-app/src/data.ts
+++ expense-app/src/data.ts
@@ -1,12 +1,39 @@
-export const data: DataType = {
-  report: [],
-};
-const MappedReport = {
+export const MappedReport = {
   income: 'income',
   expense: 'expense',
 } as const;
 
-type MappedReportLiteralType = typeof MappedReport[keyof typeof MappedReport];
+export const data: DataType = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: MappedReport.income,
+    },
+    {
+      id: 'uuid2',
+      source: 'YouTube',
+      amount: 2500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: MappedReport.income,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: MappedReport.expense,
+    },
+  ],
+};
+
+export type MappedReportLiteralType =
+  typeof MappedReport[keyof typeof MappedReport];
 
 type DataType = {
   report: {
