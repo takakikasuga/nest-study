a67378b7534c3e1c3127ebb82ea6e22ca64a1a9c
takakikasuga <takaki.soccer1@gmail.com>
2022-05-20 08:06:33 +0900

    :sparkles: メソッドに対するエンドポイントの作成

--- expense-app/src/app.controller.ts
+++ expense-app/src/app.controller.ts
@@ -1,16 +1,31 @@
 // NOTE: Controllerの責務はエンドポイントを作成すること。
 
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
 
 @Controller('report/:type')
 export class AppController {
   @Get()
-  getAllIncomeReports() {
+  getAllReports() {
     return [];
   }
 
   @Get(':id')
-  getAllIncomeReports2() {
+  getReportById() {
     return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put(':id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete(':id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
