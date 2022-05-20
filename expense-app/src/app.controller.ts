c5736e7e6625ce4f9533932d34a6827937f74ec6
takakikasuga <takaki.soccer1@gmail.com>
2022-05-20 21:28:22 +0900

    :sparkles: データ更新ロジックを実装

--- expense-app/src/app.controller.ts
+++ expense-app/src/app.controller.ts
@@ -1,6 +1,15 @@
 // NOTE: Controllerの責務はエンドポイントを作成すること。
 
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 import { data, MappedReport } from './data';
 
 @Controller('report/:type')
@@ -28,13 +37,51 @@ export class AppController {
   }
 
   @Post()
-  createReport() {
-    return 'Created';
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    console.table({ amount, source, type });
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type:
+        type === MappedReport.expense
+          ? MappedReport.expense
+          : MappedReport.income,
+    };
+    data.report.push(newReport);
+    return newReport;
   }
 
   @Put(':id')
-  updateReport() {
-    return 'Updated';
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    console.table({ id, ...body, type });
+    const reportType =
+      type === MappedReport.expense
+        ? MappedReport.expense
+        : MappedReport.income;
+
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+    if (!reportToUpdate) return;
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+    return data.report[reportIndex];
   }
 
   @Delete(':id')
