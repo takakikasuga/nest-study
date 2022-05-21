c7919427c5a8569cdf89b84061d0725b683bc289
takakikasuga <takaki.soccer1@gmail.com>
2022-05-21 15:04:54 +0900

    :sparkles: データ削除ロジックを実装

--- expense-app/src/app.controller.ts
+++ expense-app/src/app.controller.ts
@@ -5,6 +5,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpCode,
   Param,
   Post,
   Put,
@@ -84,8 +85,13 @@ export class AppController {
     return data.report[reportIndex];
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
-    return 'Deleted';
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+    if (reportIndex === -1) return;
+    data.report.splice(reportIndex, 1);
+
+    return;
   }
 }
