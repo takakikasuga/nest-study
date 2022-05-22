8a8eeac482c9a2896305774f9abd7c5960be0c31
takakikasuga <takaki.soccer1@gmail.com>
2022-05-23 07:54:30 +0900

    :sparkles: 他のモジュールのロジックを取り込んで使用する。

--- expense-app/src/summary/summary.service.ts
+++ expense-app/src/summary/summary.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { ReportService } from 'src/report/report.service';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(private readonly reportService: ReportService) {}
+
+  calculateSummary = () => {
+    return;
+  };
+}
