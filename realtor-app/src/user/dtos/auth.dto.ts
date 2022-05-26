cccb11ec6cbfe1ff5a1da2db5a38ebbbe9385999
takakikasuga <takaki.soccer1@gmail.com>
2022-05-27 08:35:11 +0900

    :sparkles: realtor-appのバリデーション機能の大元を実装

---
+++ realtor-app/src/user/dtos/auth.dto.ts
@@ -0,0 +1,25 @@
+import {
+  IsString,
+  IsNotEmpty,
+  Matches,
+  IsEmail,
+  MinLength,
+} from 'class-validator';
+
+export class SignupDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @Matches(/^(0{1}\d{9,10})$/, {
+    message: 'phone must be a valid phone number',
+  })
+  phone: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(5)
+  password: string;
+}
