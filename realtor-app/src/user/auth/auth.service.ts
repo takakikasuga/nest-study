61c1813782c1c601ea76f2f8036156315d553dcd
takakikasuga <takaki.soccer1@gmail.com>
2022-05-28 10:14:22 +0900

    :sparkles: userの作成と DB保存する場合のハッシュパスワードの生成

--- realtor-app/src/user/auth/auth.service.ts
+++ realtor-app/src/user/auth/auth.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, ConflictException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import * as bcrypt from 'bcryptjs';
+import { UserType } from '@prisma/client';
 
 type SignupParams = {
   email: string;
@@ -11,7 +13,7 @@ type SignupParams = {
 @Injectable()
 export class AuthService {
   constructor(private readonly prismaService: PrismaService) {}
-  async signup({ email }: SignupParams) {
+  async signup({ email, name, phone, password }: SignupParams) {
     const userExists = await this.prismaService.user.findUnique({
       where: { email },
     });
@@ -19,6 +21,17 @@ export class AuthService {
     if (userExists) {
       throw new ConflictException();
     }
-    console.log({ userExists });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = await this.prismaService.user.create({
+      data: {
+        email,
+        name,
+        phone,
+        password: hashedPassword,
+        userType: UserType.BUYER,
+      },
+    });
+    return user;
   }
 }
